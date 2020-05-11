13028fdb086a9a768396805a930e008022b6fc02
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-05-10 20:23:33 -0400

    ThemePicker: Add privapp whitelist for customization
    Change-Id: I38e189f6cd3db6cca8072e6c61047151960c9cf2

--- Android.mk
+++ Android.mk
@@ -31,6 +31,7 @@ LOCAL_PROGUARD_ENABLED := disabled
 
 LOCAL_PACKAGE_NAME := ThemePicker
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_REQUIRED_MODULES += privapp_whitelist_com.android.customization.xml
 LOCAL_PRODUCT_MODULE := true
 LOCAL_OVERRIDES_PACKAGES := WallpaperPicker2
 
@@ -51,6 +52,13 @@ LOCAL_MANIFEST_FILE := AndroidManifest.xml
 
 include $(BUILD_PACKAGE)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_com.android.customization.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
 # ==================================================
 include $(call all-makefiles-under,$(LOCAL_PATH))
